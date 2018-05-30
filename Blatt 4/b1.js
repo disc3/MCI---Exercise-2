8bfdeea3a839a51849105263ea26a3bef2cbb6b9
Diego Schaich <st151434@stud.uni-stuttgart.de>
2018-05-30 14:29:35 +0200

    Initial commit

--- Blatt 4/b1.js
+++ Blatt 4/b1.js
@@ -5,6 +5,6 @@ function draw() {
     deviceOrientation = 'LANDSCAPE';
     createCanvas(displayWidth, displayHeight);
     background(0);
-    fill(255,0,0);
+    fill(255);
     ellipse(50, 50, 80, 80);
 }
