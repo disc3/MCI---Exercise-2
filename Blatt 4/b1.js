9b6205890281f494f9e15e8a4742a72c6b9fa6dc
Diego Schaich <st151434@stud.uni-stuttgart.de>
2018-05-30 14:18:59 +0200

    Initial commit

---
+++ Blatt 4/b1.js
@@ -0,0 +1,10 @@
+var horizontal;
+var vertical;
+
+function draw() {
+    deviceOrientation = 'LANDSCAPE';
+    createCanvas(displayWidth, displayHeight);
+    background(0);
+    fill(255,0,0);
+    ellipse(50, 50, 80, 80);
+}
