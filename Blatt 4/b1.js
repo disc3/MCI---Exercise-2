920f03269f116d9abcd663f8e39a3872d5b85fca
Diego Schaich <st151434@stud.uni-stuttgart.de>
2018-05-30 14:52:04 +0200

    Added canvas

--- Blatt 4/b1.js
+++ Blatt 4/b1.js
@@ -5,6 +5,7 @@ function setup() {
     ScreenOrientationManager.lockOrientation('landscape-primary');
 }
 function draw() {
+    createCanvas(windowWidth, windowHeight);
     background(100);
     fill(255, 0, 0);
     line(0, height / 2, width, height / 2);
