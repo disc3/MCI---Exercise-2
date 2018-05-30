d87a882f631948ff1886365e427358c58a88c633
Diego Schaich <st151434@stud.uni-stuttgart.de>
2018-05-30 14:36:05 +0200

    Changed width argument

--- Blatt 4/b1.js
+++ Blatt 4/b1.js
@@ -3,8 +3,8 @@ var vertical;
 
 function draw() {
     deviceOrientation = 'LANDSCAPE';
-    createCanvas(displayWidth, displayHeight);
+    createCanvas(windowWidth, windowHeight);
     background(0);
-    fill(255);
+    fill(255,0,0);
     ellipse(50, 50, 80, 80);
 }
