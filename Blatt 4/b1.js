67b39ed4620e5ce654595f3712b5207812016642
Diego Schaich <st151434@stud.uni-stuttgart.de>
2018-05-30 18:21:53 +0200

    Random position

--- Blatt 4/b1.js
+++ Blatt 4/b1.js
@@ -6,6 +6,10 @@ var lastPos = {
     x: 0,
     y: 0
 };
+var nextSize = {
+    hor: 0,
+    vert: 0
+};
 var rectSizes = [{
     hor: 20,
     vert: 10,
@@ -28,41 +32,43 @@ var rectSizes = [{
     count: 0
 }];
 
-function setup() {
-
-}
-
 function draw() {
-    createCanvas(windowWidth, windowHeight);
-    background(100);
-    fill(255, 0, 0);
-    line(0, height / 2, width, height / 2);
-    calcRandomPosition();
+        createCanvas(windowWidth, windowHeight);
+        background(100);
+        fill(255, 0, 0);
+        line(0, height / 2, width, height / 2);
+        calcRandomPosition();
+
+        // draw rectangle to screen
+        fill(255, 0, 0);
+        rect(nextPos.x, nextPos.y, nextSize.hor, nextSize.vert);
 }
 
 function calcRandomPosition() {
     const MAX_COUNT = 10;
     const MIN_DISTANCE = 30;
-
-    let nextSize;
     let rand;
 
     // select random size. Check max amount while selecting
+    console.log("Array size: " + rectSizes.length - 1);
+
     do {
-        rand = random(sizes.lenght);
-    } while (rectSizes[rand].count != MAX_COUNT);
+        rand = int(random(rectSizes.length));
+        console.log("Rand index: " + rand);
+    } while (rectSizes[rand].count == MAX_COUNT);
 
     nextSize = rectSizes[rand];
     // calc random position that complies with distance premise.
     do {
-        nextPos.x = (random(width) + rectSizes[rand].hor) % width;
-    } while (int(dist(lastPos.hor, lastPos.vert, nextPos.hor, lastPos.vert)) < MIN_DISTANCE);
+        nextPos.x = int((random(width) + rectSizes[rand].hor) % width);
+        console.log("Next x position: " + nextPos.x);
+    } while (int(dist(lastPos.x, lastPos.y, nextPos.x, lastPos.y)) < MIN_DISTANCE);
     // align at middle of height
-    nextPos.y = height/2 - rectSizes[rand].vert/2;
-    // draw rectangle to screen
-    rect(nextPos.x, nextPos.y, nextSize.hor, nextSize.vert);
+    nextPos.y = height / 2 - rectSizes[rand].vert / 2;
+
 
     rectSizes[rand].count++;
     lastPos.x = nextPos.x;
     lastPos.y = nextPos.y;
 }
+
