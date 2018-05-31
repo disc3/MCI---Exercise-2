2f828a72b72265850c57356638847e11cf5edbf5
Diego Schaich <st151434@stud.uni-stuttgart.de>
2018-05-31 18:59:56 +0200

    Detect input device

--- Blatt 4/b1.js
+++ Blatt 4/b1.js
@@ -33,7 +33,10 @@ var rectSizes = [{
 }];
 var touchedShape = false;
 var turnCounter = 1;
-
+var timer;
+var distance;
+var firstTurn;
+var inputDevice;
 /*
     These constants were determined by analyzing web pages for a desktop solution of 1920x1080.
     The max height has been reduced because the task bar is always visible (size for default task bar in Windows 10).
@@ -43,12 +46,18 @@ const MAX_DESKTOP_HEIGHT = 974;
 
 
 function setup() {
-    createCanvas(min(windowWidth, MAX_DESKTOP_WIDTH), min(windowHeight, MAX_DESKTOP_HEIGHT));
-    background(100);
-    calcRandomPosition();
-    fill(255, 0, 0);
-    rect(nextPos.x, nextPos.y, nextSize.hor, nextSize.vert);
+    // detect input device
+    if(window.matchMedia('handheld').matches){
+        device = 'Handheld';
+    } else {
+        device = 'Desktop / Laptop';
+    }
     screen.orientation.lock('landscape');
+    // for the first distance. Saves the position of the cursor when the page gets loaded.
+    lastPos.x = mouseX;
+    lastPos.y = mouseY;
+    firstTurn = true;
+    distance = 0;
 }
 
 function draw() {
@@ -56,9 +65,12 @@ function draw() {
     background(100);
     fill(255, 0, 0);
     line(0, height / 2, width, height / 2);
-    // check if mouse clicked on shape. If yes, reshape.
-    if (touchedShape == true && turnCounter <=50) {
+    
+    // Re-calc position whenever the shape's been clicked. Also calc if experiment runs for first time (i.e. first turn)
+    if ((touchedShape == true && turnCounter <=50) || firstTurn) {
+        firstTurn = false;
         calcRandomPosition();
+        timer = performance.now();
     } else if(turnCounter > 50){
         alert("You finished the exercise!");
         remove();
@@ -75,29 +87,32 @@ function calcRandomPosition() {
     const MAX_COUNT = 10;
     const MIN_DISTANCE = 30;
     let randomIndex;
-    // select random size. Check max amount while selecting
-    console.log("Array size: " + rectSizes.length - 1);
-    
+
+    // protective clause to avoid infinite loop (because all counters are already maxed out)
     if (turnCounter > 50){
         alert("You finished the exercise.");
+        return;
     }
 
+    // select random size. Check max amount while selecting
     do {
         randomIndex = int(random(rectSizes.length));
-        console.log("Rand index: " + randomIndex);
     } while (rectSizes[randomIndex].count == MAX_COUNT);
 
     nextSize = rectSizes[randomIndex];
-    // calc random position that complies with distance premise.
+    // calc random position while guaranteeing that the entire shape is visible on the screen.
     do {
-        nextPos.x = int((random(width) + rectSizes[randomIndex].hor) % width);
-        console.log("Next x position: " + nextPos.x);
+        nextPos.x = int(random(width - nextSize.hor) % width);
     } while (int(dist(lastPos.x, lastPos.y, nextPos.x, lastPos.y)) < MIN_DISTANCE);
     // align at middle of height
-    nextPos.y = height / 2 - rectSizes[randomIndex].vert / 2;
-
-
+    nextPos.y = height / 2 - nextSize.vert / 2;
     rectSizes[randomIndex].count++;
+    if (turnCounter != 1){
+        distance = dist(lastPos.x, lastPos.y, nextPos.x, nextPos.y);
+    }
+    
+    console.log("Distance between shapes: " + distance);
+    
     lastPos.x = nextPos.x;
     lastPos.y = nextPos.y;
 }
@@ -106,5 +121,7 @@ function mousePressed() {
     if ((mouseX >= nextPos.x) && (mouseX <= (nextPos.x + nextSize.hor)) && (mouseY >= nextPos.y) && (mouseY <= (nextPos.y + nextSize.vert))) {
         touchedShape = true;
         turnCounter++;
+        timer = performance.now() - timer;
+        console.log("It took " + timer + " ms to click the shape.");
     }
 }
